bc20e7f02c99089517e03dc530a8092f4c0c04d5
Ben Wigley <benwigley2@gmail.com>
2019-08-08 11:45:39 +1200

    Setup stylus and create basic homepage route

--- components/nav.js
+++ components/nav.js
@@ -1,59 +1,49 @@
-import React from 'react'
+import { Component } from 'react'
 import Link from 'next/link'
+import css from '../styles/Nav.styl'
 
-const links = [
-  { href: 'https://github.com/segmentio/create-next-app', label: 'Github' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
 
-const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link prefetch href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <ul>
-        {links.map(({ key, href, label }) => (
-          <li key={key}>
-            <Link href={href}>
-              <a>{label}</a>
+class Nav extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  renderLinksList() {
+    const links = [
+      {
+        href: 'https://github.com/benwigley',
+        label: 'Github'
+      },
+      {
+        href: 'http://benwigley.com',
+        label: 'Ben Wigley'
+      }
+    ]
+    return (
+      links.map(link => {
+        return (
+          <div
+            key={`nav-link-${link.href}-${link.label}`}
+            className={css.navLink}>
+            <Link prefetch href={link.href}>
+              <a>{link.label}</a>
             </Link>
-          </li>
-        ))}
-      </ul>
-    </ul>
+          </div>
+        )
+      })
+    )
+  }
 
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
-  </nav>
-)
+  render() {
+    return (
+      <nav className={css.nav}>
+        {this.renderLinksList()}
+      </nav>
+    )
+  }
+}
 
 export default Nav
--- next.config.js
+++ next.config.js
@@ -1,10 +1,49 @@
-module.exports = {
+const path = require('path')
+const nib = require('nib')
+const withPlugins = require('next-compose-plugins')
+const withStylus = require('@zeit/next-stylus')
+
+
+// Next.js configuration
+// ---------------------
+const nextJsConfiguration = {
+  // target: 'serverless',
+  // useFileSystemPublicRoutes: false,
+
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+    config.node = { fs: 'empty' }
 
     return config
   }
 }
+
+
+// Next.js plugins
+// ---------------
+
+const nextJsPlugins = [
+
+  [withStylus, {
+    // next-stylus options
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: '[local]___[hash:base64:5]'
+    },
+    stylusLoaderOptions: {
+      use: [
+        nib()
+      ],
+      import: [
+        path.resolve(__dirname, 'styles/imports/variables.styl'),
+        // path.resolve(__dirname, 'styles/imports/mixins.styl'),
+      ],
+    },
+  }]
+
+]
+
+
+// Finally, export the final result
+module.exports = withPlugins(nextJsPlugins, nextJsConfiguration)
