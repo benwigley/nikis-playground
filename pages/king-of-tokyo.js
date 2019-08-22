61819b7a1f04abb8eb41c3e2e5c2930a2bc3dde2
Ben Wigley <benwigley2@gmail.com>
2019-08-21 19:50:05 -0700

    Add dark theme for King of Tokyo route

--- pages/king-of-tokyo.js
+++ pages/king-of-tokyo.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import { Component } from 'react'
 import Head from '../components/Head'
 import Nav from '../components/Nav'
 import '../styles/global/base.styl'
 import KingOfTokyoGame from '../components/KingOfTokyo/KingOfTokyoGame'
 
 
-const KingOfTokyoPage = () => (
-  <div>
-    <Head title="King of Tokyo" />
-    <Nav />
+export default class KingOfTokyoPage extends Component {
 
-    <div>
-      <KingOfTokyoGame />
-    </div>
-  </div>
-)
+  componentDidMount() {
+    // Set the theme for our route to Dark by adding
+    // the "dark" classname to the base <html> tag.
+    document.getElementsByTagName('html')[0].classList.add('dark')
+  }
 
-export default KingOfTokyoPage
+  componentWillUnmount() {
+    // Remove the dark theme when leaving this route.
+    // If the next rlute also want to use a theme, they can
+    // set a theme className the same way that we have done here.
+    document.getElementsByTagName('html')[0].classList.remove('dark')
+  }
+
+  render() {
+    return (
+      <div>
+        <Head title="King of Tokyo" />
+        <Nav />
+
+        <div>
+          <KingOfTokyoGame />
+        </div>
+      </div>
+    )
+  }
+
+}
