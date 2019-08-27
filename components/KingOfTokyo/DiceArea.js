6beb8e589b9916f28123e332806b9cb063ab80fd
Ben Wigley <benwigley2@gmail.com>
2019-08-26 22:59:16 -0500

    Move roll logic from DiceArea to parent component

--- components/KingOfTokyo/DiceArea.js
+++ components/KingOfTokyo/DiceArea.js
@@ -3,47 +3,12 @@ import Dice from './Dice'
 
 import css from '../../styles/KingOfTokyo/DiceArea.styl'
 
-const ONE = 'one'
-const TWO = 'two'
-const THREE = 'three'
-const HEART = 'heart'
-const ENERGY = 'energy'
-const ATTACK = 'attack'
-
-const DICE_LOOKUP = [
-  ONE,
-  TWO,
-  THREE,
-  HEART,
-  ENERGY,
-  ATTACK,
-]
 
 export default class DiceArea extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-      roll: []
-    }
-  }
-
-  generateRandomDiceRoll() {
-    return {
-      key: Math.random(),
-      value: Math.ceil(Math.random() * 5)
-    }
-  }
-
-  handleDiceRoll = () => {
-    let newRoll = []
-    var times = 6
-    for (var i = 0; i < times; i++) {
-      newRoll.push(this.generateRandomDiceRoll())
-    }
-    this.setState({
-      roll: newRoll
-    })
+    this.state = {}
   }
 
   render() {
@@ -54,12 +19,12 @@ export default class DiceArea extends Component {
         <Dice diceNumber={randomDiceRoll.value} key={randomDiceRoll.key} />
       )
     }
-    this.state.roll.forEach(createAndAddNewDiceComponent)
+    this.props.roll.forEach(createAndAddNewDiceComponent)
     
     return (
       <div className={css.diceArea}>
-        {this.state.roll.length === 0 ? (
-          <button onClick={this.handleDiceRoll}>
+        {this.props.roll.length === 0 ? (
+          <button onClick={this.props.onDiceRollClick}>
             Roll!
           </button>
         ) : diceComponents}
--- components/KingOfTokyo/KingOfTokyoGame.js
+++ components/KingOfTokyo/KingOfTokyoGame.js
@@ -7,6 +7,22 @@ import Player from './Player'
 import css from '../../styles/KingOfTokyo/KingOfTokyoGame.styl'
 
 
+const ONE = 'one'
+const TWO = 'two'
+const THREE = 'three'
+const HEART = 'heart'
+const ENERGY = 'energy'
+const ATTACK = 'attack'
+
+const DICE_LOOKUP = [
+  ONE,
+  TWO,
+  THREE,
+  HEART,
+  ENERGY,
+  ATTACK,
+]
+
 export default class KingOfTokyoGame extends Component {
 
   constructor(props) {
@@ -57,6 +73,54 @@ export default class KingOfTokyoGame extends Component {
     })
   }
 
+  handleDiceRoll = () => {
+    console.log('KingOfTokyoGame:handleDiceRoll')
+
+    // Generate a new random roll with 6 dice
+    let newRoll = []
+    var times = 6
+    for (var i = 0; i < times; i++) {
+      newRoll.push(this.generateRandomDiceRoll())
+    }
+
+    // Clone current turns array
+    let newTurnsArray = [ ...this.state.turns ]
+
+    // Find and clone the current turn
+    let currentTurn = { ...newTurnsArray[newTurnsArray.length - 1] }
+
+    // Add a new roll to the current turn's rolls array
+    currentTurn.rolls.push(newRoll)
+
+    // Replace the currentTurn
+    newTurnsArray[newTurnsArray.length - 1] = currentTurn
+
+    this.setState({ turns: newTurnsArray })
+  }
+
+  generateRandomDiceRoll() {
+    return {
+      key: Math.random(),
+      value: Math.ceil(Math.random() * 5)
+    }
+  }
+
+  getCurrentTurn() {
+    return this.state.turns[this.state.turns.length - 1]
+  }
+
+  getCurrentRoll() {
+    const currentTurn = this.getCurrentTurn()
+    if (!currentTurn) {
+      return []
+    }
+    if (currentTurn.rolls.length === 0) {
+      return []
+    } else {
+      return currentTurn.rolls[currentTurn.rolls.length - 1]
+    }
+  }
+
   render() {
     return (
       <div className={css.kingOfTokyoGame}>
@@ -64,7 +128,9 @@ export default class KingOfTokyoGame extends Component {
 
         <GameBoardArea />
 
-        <DiceArea />
+        <DiceArea 
+          roll={this.getCurrentRoll()}
+          onDiceRollClick={this.handleDiceRoll} />
 
         <div className={css.playerCardsContainer}>
           <Player
